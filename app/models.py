19307b9bd6bb43b9eccb85c704ab9d8094219d89
jackeylv(吕林) <jackeylv@tencent.com>
2016-03-23 19:55:40 +0800

    db

--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
+# coding=utf-8
+# 从项目自身app.__init__.py中import db这个变量
 from app import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    # 一个新的字段称为 posts，它是被构建成一个 db.relationship 字段。这并不是一个实际的数据库字段
+    # 对于一个一对多的关系，db.relationship 字段通常是定义在“一”这一边。在这种关系下，我们得到一个 user.posts 成员，它给出一个用户所有的 blog
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % self.nickname
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    # user_id 字段初始化成外键
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.body
--- db_create.py
+++ db_create.py
@@ -1,9 +1,13 @@
 #!flask/bin/python
+# coding=utf-8
 from migrate.versioning import api
+# 从自身模块中import以下三个变量
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
+
 import os.path
+# 创建表
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
